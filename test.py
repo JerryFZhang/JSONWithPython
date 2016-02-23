01ee2c53b63354a3f29b00cf3c4469b5c7162f60
zhang96 <zhang96@users.noreply.github.com>
2016-02-23 12:42:59 -0500

    Init
    Almost there...

---
+++ test.py
@@ -0,0 +1,29 @@
+import json
+from pprint import pprint
+
+with open('products.json') as data_file:
+    data = json.load(data_file)
+
+productsArr = data['products']
+computerArr = []
+keyboardArr = []
+
+
+for item in productsArr:
+    string = str(item)
+    index = str(productsArr.index(item))
+    if "computer" in string:
+        print ("Item "+ index + " is a computer.")
+        computerArr.append(item)
+    else:
+         pass
+
+    if "keyboard" in string:
+        print ("Item "+ index + " is a keyboard.")
+        keyboardArr.append(item)
+
+    else:
+         pass
+
+
+print ("There is(are) " + str(len(computerArr)) + " computer(s) " + str(len(keyboardArr)) + " keyboard(s)." )
