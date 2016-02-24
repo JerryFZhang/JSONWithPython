cf5e6eb5ce6439197e9c11c671eca5efb613f308
zhang96 <zhang96@users.noreply.github.com>
2016-02-24 17:48:07 -0500

    Another more efficient solution

---
+++ test2.py
@@ -0,0 +1,16 @@
+import json
+import itertools
+
+with open('products.json') as data_file:
+    data = json.load(data_file)
+    products = data['products']
+
+
+products_temp = []
+
+for index, item in enumerate(products)
+    products_temp.append(item)
+products = products_temp
+products_temp = None # delete the variable
+for index item in enumerate(products)
+    products_temp.append(item[varients])
