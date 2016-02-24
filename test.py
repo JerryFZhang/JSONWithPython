59eb33b7543e4ad86eeaa4636ae9f10ebdfc04ad
zhang96 <zhang96@users.noreply.github.com>
2016-02-23 19:34:23 -0500

    Data appended in a  new array.

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import json
-from pprint import pprint
+# from pprint import pprint
 
 with open('products.json') as data_file:
     data = json.load(data_file)
+    productsArr = data['products']
 
-productsArr = data['products']
 computerArr = []
 keyboardArr = []
 
@@ -20,24 +20,39 @@ for index, item in enumerate(productsArr):
          pass
 
 computerVarientsArr = []
-keyboardVarientsArr = []
+
 for item in computerArr:
     computerVarientsArr.append(item['variants'])
+
+keyboardVarientsArr = []
+
 for item in keyboardArr:
     keyboardVarientsArr.append(item['variants'])
 
 computerDetailsArr = []
-keyboardDetailsArr = []
 
 for index, item in enumerate(computerVarientsArr):
     for index, item in enumerate(item):
         computerDetailsArr.append(item)
 
+keyboardDetailsArr = []
+
 for index, item in enumerate(keyboardVarientsArr):
     for index, item in enumerate(item):
         keyboardDetailsArr.append(item)
 
+computerWeightArr = []
+computerPriceArr = []
 
 for item in computerDetailsArr:
-    print (item['grams'])
-    print (item['price'])
+    computerWeightArr.append(item['grams'])
+    computerPriceArr.append(item['price'])
+
+keyboardWeightArr = []
+keyboardPriceArr = []
+
+for item in keyboardDetailsArr:
+    keyboardWeightArr.append(item['grams'])
+    keyboardPriceArr.append(item['price'])
+
+print computerPriceArr
